15b7c088d55f476c42ba0cb7bcfee2886f6f3906
Hyungsuk Kang <hskang9@gmail.com>
2024-10-30 06:13:28 +0900

    add test cases

--- test/DFV.t.sol
+++ test/DFV.t.sol
@@ -7,5 +7,17 @@ import {DFV} from "../src/DFV.sol";
 contract DFVTest is Test {
     DFV public dfv;
 
+    // DAO can set exchange whitelist
+
+    // DAO can set allowed sell amount
+
+    // DAO can set OTC whitelist amount
+
+    // DAO can set OTC whitelist to all receivers in case of token sale with limited amount
+
     // DFV can send tokens with OTC whitelist
+
+    // DFV can be minted with only max supply
+
+    // DFV has penalty logic on unauthorized sales
 }
